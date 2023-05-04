aca016588504a37ccd2035d7e88bfa5b1a280b34
fanatiker1990 <59375292+fanatiker1990@users.noreply.github.com>
2023-05-04 18:52:23 +0300

    1

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,7 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int numberMax= (first > second&&first>third)?  numberMax=first:( (second>first&&second>third)? (numberMax=second):(numberMax=third));
+        System.out.println(numberMax);
     }
 }
